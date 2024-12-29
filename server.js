437e74a7878ca19cc569c8c3373cc539cb581bcb
Asad Saeed <asadsaeed@gsoftconsulting.com>
2024-12-29 20:15:33 +0500

    feat:creating routes and responses in express

--- server.js
+++ server.js
@@ -8,8 +8,40 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.send("Hello " + process.env.NODE_ENV + " mode");
+app.get("/api/v1/bootcamps", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Show all bootcamps",
+    data: { name: "Asad Saeed" },
+  });
+});
+
+app.get("/api/v1/bootcamps/:id", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: `Show bootcamp ${req.params.id}`,
+  });
+});
+
+app.post("/api/v1/bootcamps", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Create new bootcamps",
+  });
+});
+
+app.put("/api/v1/bootcamps/:id", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: `Update bootcamp ${req.params.id}`,
+  });
+});
+
+app.delete("/api/v1/bootcamps/:id", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: `Delete bootcamps ${req.params.id}`,
+  });
 });
 
 app.listen(PORT, () => {
