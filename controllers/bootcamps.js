8ccf705f7ef07097fda2907a0761b99372ee9e05
Asad Saeed <asadsaeed@gsoftconsulting.com>
2025-01-16 22:07:52 +0500

    feat:update bootcamp controller

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -66,11 +66,29 @@ export const createBootcamp = async (req, res, next) => {
 // @desc Update bootcamp
 // @route PUT /api/v1/bootcamps/:id
 // @access Private
-export const updateBootcamp = (req, res, next) => {
-  res.status(200).json({
-    success: true,
-    message: `Update bootcamp ${req.params.id}`,
-  });
+export const updateBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!bootcamp) {
+      return res.status(400).json({
+        success: false,
+        message: "Bootcamp not found",
+      });
+    }
+    return res.status(200).json({
+      success: true,
+      message: `Bootcamp updated successfully`,
+      data: bootcamp,
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      message: error.message,
+    });
+  }
 };
 
 // @desc Delete bootcamp
