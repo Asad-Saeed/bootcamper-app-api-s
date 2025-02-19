63d1c433ea4f2c936a174b704b038e20080f798c
MN <mn@MNs-MacBook-Pro.local>
2025-02-20 00:42:09 +0500

    feat:forgot password and generate token

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 import User from "../models/User.js";
 import asyncHandler from "../middleware/async.js";
 import ErrorResponse from "../utils/errorResponse.js";
+import crypto from "crypto";
 
 //@desc Register user
 //@route POST /api/v1/auth/register
@@ -48,6 +49,46 @@ export const login = asyncHandler(async (req, res, next) => {
   sendTokenResponse(user, 200, "User logged in successfully", res);
 });
 
+// @desc POST /api/v1/auth/forgotpassword
+// @route POST /api/v1/auth/forgotpassword
+// @access Public
+export const forgotPassword = asyncHandler(async (req, res, next) => {
+  const user = await User.findOne({ email: req.body.email });
+
+  if (!user) {
+    return next(new ErrorResponse("User not found", 404));
+  }
+
+  // Get reset token
+  const resetToken = user.getResetPasswordToken();
+
+  // Set expire time
+  const resetExpire = Date.now() + 10 * 60 * 1000;
+
+  // Create reset password token
+  const resetPasswordToken = crypto.randomBytes(20).toString("hex");
+
+  await user.save({ validateBeforeSave: false });
+
+  res.status(200).json({
+    success: true,
+    message: "Password reset token sent to email",
+    data: user,
+  });
+});
+
+//@desc Get current logged in user
+//@route GET /api/v1/auth/me
+//@access Private
+export const getMe = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.user.id);
+  res.status(200).json({
+    success: true,
+    message: "User fetched successfully",
+    data: user,
+  });
+});
+
 // Get token from model, create cookie and send response
 const sendTokenResponse = (user, statusCode, message, res) => {
   // Create token
@@ -67,15 +108,3 @@ const sendTokenResponse = (user, statusCode, message, res) => {
     token,
   });
 };
-
-//@desc Get current logged in user
-//@route GET /api/v1/auth/me
-//@access Private
-export const getMe = asyncHandler(async (req, res, next) => {
-  const user = await User.findById(req.user.id);
-  res.status(200).json({
-    success: true,
-    message: "User fetched successfully",
-    data: user,
-  });
-});
