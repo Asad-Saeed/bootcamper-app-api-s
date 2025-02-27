43176632931a1879db3cf1cb33544484e05eb479
MN <mn@MNs-MacBook-Pro.local>
2025-02-27 21:00:43 +0500

    feat:aggregate and calculate avg rating

--- models/Review.js
+++ models/Review.js
@@ -32,4 +32,31 @@ const ReviewSchema = new mongoose.Schema(
 // Prevent user from submitting multiple reviews for the same bootcamp
 ReviewSchema.index({ bootcamp: 1, user: 1 }, { unique: true });
 
+// Static method to get average rating and save
+ReviewSchema.statics.getAverageRating = async function (bootcampId) {
+  const obj = await this.aggregate([
+    { $match: { bootcamp: bootcampId } },
+    { $group: { _id: "$bootcamp", averageRating: { $avg: "$rating" } } },
+  ]);
+
+  // Update bootcamp with the average rating
+  try {
+    await this.model("Bootcamp").findByIdAndUpdate(bootcampId, {
+      averageRating: obj[0].averageRating,
+    });
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+// Update bootcamp with the average rating after saving
+ReviewSchema.post("save", function () {
+  this.constructor.getAverageRating(this.bootcamp);
+});
+
+// Update bootcamp with the average rating before removing
+ReviewSchema.pre("remove", function () {
+  this.constructor.getAverageRating(this.bootcamp);
+});
+
 export default mongoose.model("Review", ReviewSchema);
